0189f719079ebf20f0110103981e8263da0146c3
Kirill Borisov <lensvol@gmail.com>
2022-12-28 14:57:26 +0100

    Proof of Concept.

---
+++ src/main.rs
@@ -0,0 +1,146 @@
+use html_parser::{Dom, Element, Node};
+use std::collections::{HashMap, VecDeque};
+use std::io::{stdin, Read};
+use std::{env, fs, str};
+
+struct NodeScriptifier {
+    registry: HashMap<String, u8>,
+}
+
+impl NodeScriptifier {
+    fn new() -> NodeScriptifier {
+        NodeScriptifier {
+            registry: Default::default(),
+        }
+    }
+
+    fn sanitize_name(&mut self, base: &String) -> String {
+        let current_mark = self.registry.entry(base.clone()).or_insert(0);
+        *current_mark += 1;
+        return if *current_mark == 1 {
+            base.clone()
+        } else {
+            format!("{}{}", base, *current_mark)
+        };
+    }
+
+    fn scriptify(&mut self, node: &Element) -> (String, Vec<String>) {
+        let mut result: Vec<String> = Vec::new();
+
+        let name = if let Some(node_id) = &node.id {
+            node_id.clone()
+        } else {
+            node.name.clone()
+        };
+        let sanitized = self.sanitize_name(&name);
+        result.push(format!(
+            "const {} = document.createElement('{}');",
+            sanitized, &node.name
+        ));
+
+        if !node.classes.is_empty() {
+            result.push(format!(
+                "{}.classList.add('{}');",
+                sanitized,
+                node.classes.join("', '")
+            ));
+        }
+
+        for (attribute, optional_value) in &node.attributes {
+            // TODO: Maybe handle null values?
+            if let Some(value) = optional_value {
+                if attribute == "style" {
+                    result.push(format!("{}.style.cssText = '{}';", sanitized, value));
+                } else if attribute.starts_with("data-") {
+                    result.push(format!(
+                        "{}.dataset.{} = '{}';",
+                        sanitized, attribute, value
+                    ));
+                } else {
+                    result.push(format!(
+                        "{}.setAttribute('{}', '{}');",
+                        sanitized, attribute, value
+                    ));
+                }
+            }
+        }
+
+        (sanitized, result)
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut scriptifier = NodeScriptifier::new();
+    let mut relationships: Vec<(String, String)> = Vec::new();
+    let mut root_element: Option<String> = None;
+    let mut source_code: Vec<String> = Vec::new();
+    let mut buffer = Vec::new();
+
+    if args.len() < 2 {
+        println!("Usage: mimicui < - | FILE >");
+        std::process::exit(-1);
+    }
+
+    let source = args.get(1).unwrap();
+
+    if source == "-" {
+        stdin()
+            .read_to_end(&mut buffer)
+            .expect("Nothing to process!");
+    } else {
+        buffer = Vec::from(fs::read_to_string("fragment.html").unwrap());
+    }
+
+    let dom = Dom::parse(str::from_utf8(&buffer).unwrap()).unwrap();
+
+    let mut stack: VecDeque<(usize, Option<String>, &Node)> =
+        dom.children.iter().map(|n| (0, None, n)).collect();
+
+    if stack.len() > 1 {
+        source_code.push("".to_string());
+        source_code.push("const root = document.createElement('div');".to_string());
+        source_code.push("".to_string());
+
+        root_element = Some("root".to_string());
+    }
+
+    while let Some((indent, parent_name, node)) = stack.pop_front() {
+        match node {
+            Node::Text(text) => println!("{}", text),
+            Node::Element(el) => {
+                let (name, lines) = scriptifier.scriptify(el);
+                if root_element.is_none() {
+                    root_element = Some(name.clone());
+                }
+
+                lines.iter().for_each(|l| source_code.push(l.into()));
+
+                for (_, subchild) in el.children.iter().enumerate() {
+                    if let Node::Text(text) = subchild {
+                        source_code.push(format!("{}.textContent = '{}'", name, text));
+                    } else {
+                        stack.push_back((indent + 4, Some(name.clone()), subchild));
+                    }
+                }
+
+                source_code.push("".to_string());
+                if let Some(node_name) = parent_name {
+                    relationships.push((node_name, name));
+                }
+            }
+            Node::Comment(_) => {}
+        }
+    }
+
+    for (parent, child) in relationships.iter() {
+        source_code.push(format!("{}.appendChild({});", parent, child));
+    }
+    source_code.push(format!("return {};", root_element.unwrap()));
+
+    println!("function createMimic() {{");
+    source_code.iter().for_each(|l| println!("    {}", l));
+    println!("}}");
+
+    std::process::exit(0);
+}
